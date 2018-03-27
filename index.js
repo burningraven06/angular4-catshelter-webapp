5f1342baf6365a8ec338fdc84380227acfbf21d5
Toufiq <burningraven06@gmail.com>
2018-03-28 00:18:06 +0600

    seperate backend

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('./ng4alexcatshelterapp/dist/'));
+app.listen(port, () => {
+  console.log(`Express running on port: ${port}`);
+});
